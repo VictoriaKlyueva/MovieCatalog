524d40ed15aa9ea107632845a1428114545ed607
vekosek <vika_klyueva@inbox.ru>
2024-10-09 02:28:07 +0700

    Launch screen with intent into MainActivity instead of the timer

--- app/src/main/java/com/example/moviecatalog/SplashActivity.kt
+++ app/src/main/java/com/example/moviecatalog/SplashActivity.kt
@@ -2,7 +2,6 @@ package com.example.moviecatalog
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
@@ -10,10 +9,11 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Переход к основному Activity без задержки
-        Handler().postDelayed({
-            startActivity(Intent(this, MainActivity::class.java))
-            finish() // Закрываем SplashActivity
-        }, 0) // Задержка равна 0, переход произойдет немедленно
+        val intent = Intent(
+            this,
+            MainActivity::class.java
+        )
+        startActivity(intent)
+        finish()
     }
 }
