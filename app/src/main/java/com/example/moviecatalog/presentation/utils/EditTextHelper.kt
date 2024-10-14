31c499abe31cd2a0092b2ec3bc4f56b67756349f
vekosek <vika_klyueva@inbox.ru>
2024-10-15 01:30:28 +0700

    Some logic moved to EditTextHelper + refactoring

---
+++ app/src/main/java/com/example/moviecatalog/presentation/utils/EditTextHelper.kt
@@ -0,0 +1,90 @@
+package com.example.moviecatalog.utils
+
+import android.text.Editable
+import android.text.TextWatcher
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
+import android.view.MotionEvent
+import android.widget.EditText
+import androidx.annotation.DrawableRes
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.moviecatalog.R
+
+object EditTextHelper {
+
+    private val _isButtonEnabled = MutableLiveData<Boolean>()
+    val isButtonEnabled: LiveData<Boolean> get() = _isButtonEnabled
+
+    fun createTextWatcher(editText: EditText, @DrawableRes clearIcon: Int, onTextChanged: (String) -> Unit): TextWatcher {
+        return object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s.isNullOrEmpty()) {
+                    editText.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0)
+                } else {
+                    editText.setCompoundDrawablesWithIntrinsicBounds(0, 0, clearIcon, 0)
+                }
+
+                // Вызываем функцию для обработки введенных данных
+                onTextChanged(editText.text.toString())
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        }
+    }
+
+    fun setClearTextOnIconTouch(editText: EditText) {
+        editText.setOnTouchListener { _, event ->
+            if (event.action == MotionEvent.ACTION_UP) {
+                if (editText.compoundDrawables[2] != null) {
+                    if (event.rawX >= (editText.right - editText.compoundDrawables[2].bounds.width())) {
+                        editText.setText("")
+                        return@setOnTouchListener true
+                    }
+                }
+            }
+            false
+        }
+    }
+
+    fun setHidePasswordOnIconTouch(editText: EditText) {
+        editText.setOnTouchListener { _, event ->
+            if (event.action == MotionEvent.ACTION_UP) {
+                if (editText.compoundDrawables[2] != null) {
+                    if (event.rawX >= (editText.right - editText.compoundDrawables[2].bounds.width())) {
+                        if (editText.transformationMethod is HideReturnsTransformationMethod) {
+                            editText.transformationMethod = PasswordTransformationMethod.getInstance()
+                            editText.setCompoundDrawablesWithIntrinsicBounds(
+                                0,
+                                0,
+                                R.drawable.ic_show_password,
+                                0
+                            )
+                        } else {
+                            editText.transformationMethod = HideReturnsTransformationMethod.getInstance()
+                            editText.setCompoundDrawablesWithIntrinsicBounds(
+                                0,
+                                0,
+                                R.drawable.ic_hide_password,
+                                0
+                            )
+                        }
+                        editText.setSelection(editText.text.length)
+                        return@setOnTouchListener true
+                    }
+                }
+            }
+            false
+        }
+    }
+
+    fun hideIcon(editText: EditText) {
+        editText.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0)
+    }
+
+    fun updateButtonStatus(isEnabled: Boolean) {
+        _isButtonEnabled.value = isEnabled
+    }
+}
--- app/src/main/java/com/example/moviecatalog/presentation/view/Fragments/SignInFragment.kt
+++ app/src/main/java/com/example/moviecatalog/presentation/view/Fragments/SignInFragment.kt
@@ -1,13 +1,8 @@
 package com.example.moviecatalog.presentation.view.Fragments
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.text.method.HideReturnsTransformationMethod
 import android.text.method.PasswordTransformationMethod
 import android.view.LayoutInflater
-import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
@@ -17,6 +12,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.moviecatalog.R
 import com.example.moviecatalog.presentation.viewModel.SignInViewModel
+import com.example.moviecatalog.utils.EditTextHelper
 
 
 class SignInFragment : Fragment() {
@@ -27,91 +23,32 @@ class SignInFragment : Fragment() {
 
     private lateinit var viewModel: SignInViewModel
 
-    private fun createTextWatcher(editText: EditText, icon: Int, isPassword: Boolean = false): TextWatcher {
-        return object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                if (s.isNullOrEmpty()) {
-                    if (isPassword && editText.transformationMethod is HideReturnsTransformationMethod) {
-                        editText.transformationMethod = PasswordTransformationMethod.getInstance()
-                    }
-                    editText.setCompoundDrawablesWithIntrinsicBounds(
-                        0,
-                        0,
-                        0,
-                        0
-                    )
-                } else {
-                    editText.setCompoundDrawablesWithIntrinsicBounds(
-                        0,
-                        0,
-                        icon,
-                        0
-                    )
-                }
-
-                viewModel.onLoginDataChanged(editTextLogin.text.toString(), editTextPassword.text.toString())
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-            }
-        }
+    private fun editTextLoginProcessing(view: View) {
+        editTextLogin = view.findViewById(R.id.editTextLogin)
+        EditTextHelper.hideIcon(editTextLogin)
+        editTextLogin.addTextChangedListener(EditTextHelper.createTextWatcher(
+            editTextLogin,
+            R.drawable.ic_clear
+        ) { input ->
+            viewModel.onLoginDataChanged(input, editTextPassword.text.toString())
+        })
+        EditTextHelper.setClearTextOnIconTouch(editTextLogin)
     }
 
-    @SuppressLint("ClickableViewAccessibility")
-    fun setClearTextOnIconTouch(editText: EditText) {
-        editText.setOnTouchListener { v: View?, event: MotionEvent ->
-            if (event.action == MotionEvent.ACTION_UP) {
-                if (editText.compoundDrawables[2] != null) {
-                    if (event.rawX >=
-                        (editText.right - editText.compoundDrawables[2].bounds.width())) {
-                        editText.setText("")
-                        return@setOnTouchListener true
-                    }
-                }
-            }
-            false
-        }
-    }
+    private fun editTextPasswordProcessing(view: View) {
+        editTextPassword = view.findViewById(R.id.editTextPassword)
+        EditTextHelper.hideIcon(editTextPassword)
 
-    @SuppressLint("ClickableViewAccessibility")
-    private fun setHidePasswordOnIconTouch(editText: EditText) {
-        editText.setOnTouchListener { v, event: MotionEvent ->
-            if (event.action == MotionEvent.ACTION_UP) {
-                if (editText.compoundDrawables[2] != null) {
-                    if (event.rawX >= (editText.right - editText.compoundDrawables[2].bounds.width())) {
-                        if (editText.transformationMethod is HideReturnsTransformationMethod) {
-                            // Скрыть пароль
-                            editText.transformationMethod = PasswordTransformationMethod.getInstance()
-                            editText.setCompoundDrawablesWithIntrinsicBounds(
-                                0,
-                                0,
-                                R.drawable.ic_show_password,
-                                0
-                            )
-                        } else {
-                            // Показать пароль
-                            editText.transformationMethod = HideReturnsTransformationMethod.getInstance()
-                            editText.setCompoundDrawablesWithIntrinsicBounds(
-                                0,
-                                0,
-                                R.drawable.ic_hide_password,
-                                0
-                            )
-                        }
-                        editText.setSelection(editText.text.length)
-                        return@setOnTouchListener true
-                    }
-                }
-            }
-            false
-        }
-    }
+        val passwordIcon = if (editTextPassword.transformationMethod == PasswordTransformationMethod.getInstance())
+            R.drawable.ic_show_password
+        else
+            R.drawable.ic_hide_password
 
-    private fun hideIcon(editText: EditText) {
-        editText.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0)
+        editTextPassword.addTextChangedListener(
+            EditTextHelper.createTextWatcher(editTextPassword, passwordIcon) { input ->
+            viewModel.onLoginDataChanged(editTextLogin.text.toString(), input)
+        })
+        EditTextHelper.setHidePasswordOnIconTouch(editTextPassword)
     }
 
     override fun onCreateView(
@@ -122,33 +59,25 @@ class SignInFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_sign_in, container, false)
         viewModel = ViewModelProvider(this).get(SignInViewModel::class.java)
 
-        editTextLogin = view.findViewById(R.id.editTextLogin)
-        hideIcon(editTextLogin)
-        editTextLogin.addTextChangedListener(createTextWatcher(editTextLogin, R.drawable.ic_clear))
-        setClearTextOnIconTouch(editTextLogin)
-
-        editTextPassword = view.findViewById(R.id.editTextPassword)
-        hideIcon(editTextPassword)
-
-        val passwordIcon =
-            if (editTextPassword.transformationMethod == PasswordTransformationMethod.getInstance())
-                R.drawable.ic_show_password
-            else
-                R.drawable.ic_hide_password
-
-        editTextPassword.addTextChangedListener(createTextWatcher(
-            editTextPassword,
-            passwordIcon,
-            true
-        ))
-        setHidePasswordOnIconTouch(editTextPassword)
+        editTextLoginProcessing(view)
+        editTextPasswordProcessing(view)
 
         buttonSignIn = view.findViewById(R.id.buttonSignIn)
 
         viewModel.isButtonEnabled.observe(viewLifecycleOwner, Observer { isEnabled ->
+            EditTextHelper.updateButtonStatus(isEnabled)
             buttonSignIn.isEnabled = isEnabled
-            buttonSignIn.setTextColor(if (isEnabled) resources.getColor(R.color.white) else resources.getColor(R.color.gray_faded))
-            buttonSignIn.setBackgroundResource(if (isEnabled) R.drawable.button_primary_default else R.drawable.button_secondary)
+            buttonSignIn.setTextColor(
+                if (isEnabled)
+                    resources.getColor(R.color.white)
+                else resources.getColor(R.color.gray_faded)
+            )
+            buttonSignIn.setBackgroundResource(
+                if (isEnabled)
+                    R.drawable.button_primary_default
+                else
+                    R.drawable.button_secondary
+            )
         })
 
         return view
