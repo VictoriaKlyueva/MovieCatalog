218863bf5299d4a47d0bbd235af4693d78954c21
vekosek <vika_klyueva@inbox.ru>
2024-10-17 13:42:59 +0700

    Everything with binding

--- app/src/main/java/com/example/moviecatalog/presentation/view/Fragments/SignUpFragment.kt
+++ app/src/main/java/com/example/moviecatalog/presentation/view/Fragments/SignUpFragment.kt
@@ -8,9 +8,6 @@ import android.text.method.PasswordTransformationMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ToggleButton
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import com.example.moviecatalog.R
