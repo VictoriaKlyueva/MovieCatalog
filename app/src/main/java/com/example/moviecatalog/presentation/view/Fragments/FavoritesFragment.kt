7a4a8ac33d233a96b2e87b1176953f9d24057a92
vekosek <vika_klyueva@inbox.ru>
2024-10-31 04:10:47 +0700

    Refactoring

--- app/src/main/java/com/example/moviecatalog/presentation/view/Fragments/FavoritesFragment.kt
+++ app/src/main/java/com/example/moviecatalog/presentation/view/Fragments/FavoritesFragment.kt
@@ -14,14 +14,8 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -40,79 +34,31 @@ import coil.compose.rememberAsyncImagePainter
 import com.example.moviecatalog.R
 import com.example.moviecatalog.data.model.GenreModel
 import com.example.moviecatalog.data.model.MovieElementModel
-import com.example.moviecatalog.presentation.ui.DarkFaded
-import com.example.moviecatalog.presentation.ui.GradientEnd
-import com.example.moviecatalog.presentation.ui.GradientStart
-import com.example.moviecatalog.presentation.ui.GrayFaded
-import com.example.moviecatalog.presentation.ui.Theme
-import com.example.moviecatalog.presentation.ui.eightStars
-import com.example.moviecatalog.presentation.ui.fiveStars
-import com.example.moviecatalog.presentation.ui.fourStars
-import com.example.moviecatalog.presentation.ui.nineStars
-import com.example.moviecatalog.presentation.ui.oneStar
-import com.example.moviecatalog.presentation.ui.sevenStars
-import com.example.moviecatalog.presentation.ui.sixStars
-import com.example.moviecatalog.presentation.ui.threeStars
-import com.example.moviecatalog.presentation.ui.twoStars
+import com.example.moviecatalog.presentation.ui.*
 import com.example.moviecatalog.presentation.viewModel.FavoritesViewModel
 
 class FavoritesFragment : Fragment() {
 
     private val favoritesViewModel: FavoritesViewModel by viewModels()
 
-    private var favoritesGenres: List<GenreModel> = emptyList()
-    private var favoriteMovies: List<MovieElementModel> = emptyList()
+    private var favoritesGenres: List<GenreModel> by mutableStateOf(emptyList())
+    private var favoriteMovies: List<MovieElementModel> by mutableStateOf(emptyList())
 
-    private val gradientTextModifier =
-        Modifier.graphicsLayer(alpha = 0.99f)
+    private val gradientTextModifier = Modifier
+        .graphicsLayer(alpha = 0.99f)
         .drawWithCache {
-            val brush = Brush.horizontalGradient(
-                listOf(
-                    GradientStart,
-                    GradientEnd
-                )
-            )
+            val brush = Brush.horizontalGradient(listOf(GradientStart, GradientEnd))
             onDrawWithContent {
                 drawContent()
                 drawRect(brush, blendMode = BlendMode.SrcAtop)
             }
         }
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return ComposeView(requireContext()).apply {
             setContent {
-                this@FavoritesFragment.Content()
-            }
-        }
-    }
-
-    private fun observeGenres() {
-        favoritesViewModel.favoritesGenres.observe(viewLifecycleOwner) { result ->
-            when {
-                result.isNullOrEmpty() -> {
-                    println("No genres found")
-                    // Показать заглушку
-                }
-                else -> {
-                    favoritesGenres = result
-                }
-            }
-        }
-    }
-
-    private fun observeMovies() {
-        favoritesViewModel.favoritesMovies.observe(viewLifecycleOwner) { result ->
-            when {
-                result.isNullOrEmpty() -> {
-                    println("No genres found")
-                    // Показать заглушку
-                }
-                else -> {
-                    favoriteMovies = result
+                Theme {
+                    this@FavoritesFragment.Content()
                 }
             }
         }
@@ -120,111 +66,73 @@ class FavoritesFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        observeGenres()
-        observeMovies()
-
+        observeData()
         favoritesViewModel.fetchFavoritesGenres()
         favoritesViewModel.fetchFavoritesMovies()
     }
 
+    private fun observeData() {
+        favoritesViewModel.favoritesGenres.observe(viewLifecycleOwner) {
+            favoritesGenres = it ?: emptyList()
+        }
+        favoritesViewModel.favoritesMovies.observe(viewLifecycleOwner) {
+            favoriteMovies = it ?: emptyList()
+        }
+    }
+
     @Composable
     private fun Content() {
-        val favoritesGenresState = remember { mutableStateOf(emptyList<GenreModel>()) }
-        val favoritesMoviesState = remember { mutableStateOf(emptyList<MovieElementModel>()) }
-
-        favoritesViewModel.favoritesGenres.observe(viewLifecycleOwner) { result ->
-            favoritesGenresState.value = result ?: emptyList()
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(24.dp),
+            horizontalAlignment = Alignment.Start
+        ) {
+            Text(text = stringResource(id = R.string.favorite), style = MaterialTheme.typography.titleLarge)
+            Spacer(modifier = Modifier.height(32.dp))
+            GenreSection()
+            Spacer(modifier = Modifier.height(32.dp))
+            MovieSection()
         }
+    }
 
-        favoritesViewModel.favoritesMovies.observe(viewLifecycleOwner) { result ->
-            favoritesMoviesState.value = result ?: emptyList()
-        }
-
-        return Theme {
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 24.dp, start = 24.dp, end = 24.dp),
-                horizontalAlignment = Alignment.Start
-            ) {
-                Text(
-                    text = stringResource(id = R.string.favorite),
-                    style = MaterialTheme.typography.titleLarge
-                )
-
-                Spacer(modifier = Modifier.height(32.dp))
-
-                Text(
-                    modifier = gradientTextModifier,
-                    text = stringResource(id = R.string.favorite_genres),
-                    style = MaterialTheme.typography.bodyLarge
-                )
-
-                Spacer(modifier = Modifier.height(16.dp))
-
-                LazyColumn {
-                    items(favoritesGenresState.value) { genre ->
-                        GenreItem(genre.name.replaceFirstChar(Char::uppercaseChar))
-                        Spacer(modifier = Modifier.height(8.dp))
-                    }
-                }
-
-                Spacer(modifier = Modifier.height(32.dp))
-
-                Text(
-                    modifier = gradientTextModifier,
-                    text = stringResource(id = R.string.favorite_movies),
-                    style = MaterialTheme.typography.bodyLarge
-                )
-
-                LazyVerticalGrid(
-                    columns = GridCells.Fixed(3),
-                    modifier = Modifier.fillMaxWidth(),
-                ) {
-                    items(favoritesMoviesState.value) { movie ->
-                        MovieItem(movie)
-                    }
-                }
+    @Composable
+    private fun GenreSection() {
+        Text(modifier = gradientTextModifier, text = stringResource(id = R.string.favorite_genres), style = MaterialTheme.typography.bodyLarge)
+        Spacer(modifier = Modifier.height(16.dp))
+        LazyColumn {
+            items(favoritesGenres) { genre ->
+                GenreItem(genre.name.replaceFirstChar(Char::uppercaseChar))
+                Spacer(modifier = Modifier.height(8.dp))
             }
         }
     }
 
     @Composable
+    private fun MovieSection() {
+        Text(modifier = gradientTextModifier, text = stringResource(id = R.string.favorite_movies), style = MaterialTheme.typography.bodyLarge)
+        LazyVerticalGrid(columns = GridCells.Fixed(3), modifier = Modifier.fillMaxWidth()) {
+            items(favoriteMovies) { movie -> MovieItem(movie) }
+        }
+    }
+
+    @Composable
     fun GenreItem(genre: String) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .clip(RoundedCornerShape(8.dp))
-                .background(DarkFaded),
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            Text(
-                modifier = Modifier
-                    .padding(start = 16.dp)
-                    .weight(1f),
-                text = genre,
-                style = MaterialTheme.typography.bodyMedium
-            )
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(8.dp))
+            .background(DarkFaded),
+            verticalAlignment = Alignment.CenterVertically) {
+
+            Text(modifier = Modifier.padding(start = 16.dp).weight(1f), text = genre, style = MaterialTheme.typography.bodyMedium)
 
             Button(
-                modifier = Modifier
-                    .padding(end = 16.dp)
-                    .wrapContentSize()
-                    .align(Alignment.CenterVertically),
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = Color(resources.getColor(R.color.dark))
-                ),
+                modifier = Modifier.padding(end = 16.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Color(resources.getColor(R.color.dark))),
                 shape = RoundedCornerShape(8.dp),
-                onClick = {
-                    /* Логика удаления из избранного */
-                },
+                onClick = { /* Логика удаления из избранного */ }
             ) {
-                Image(
-                    painter = painterResource(id = R.drawable.ic_broken_heart),
-                    contentDescription = "Broken Heart",
-                    modifier = Modifier.size(24.dp)
-                )
+                Image(painter = painterResource(id = R.drawable.ic_broken_heart), contentDescription = "Broken Heart", modifier = Modifier.size(24.dp))
             }
         }
     }
@@ -240,65 +148,39 @@ class FavoritesFragment : Fragment() {
                 .clip(RoundedCornerShape(8.dp)),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Box(
-                modifier = Modifier.fillMaxSize()
-            ) {
-                Image(
-                    painter = rememberAsyncImagePainter(movie.poster),
-                    contentDescription = movie.name,
-                    modifier = Modifier.fillMaxSize()
-                )
-
-                // Плашка с рейтингом
+            Box(modifier = Modifier.fillMaxSize()) {
+                Image(painter = rememberAsyncImagePainter(movie.poster), contentDescription = movie.name, modifier = Modifier.fillMaxSize())
+                val averageRating = movie.reviews.map { it.rating }.average()
                 Surface(
                     modifier = Modifier
                         .padding(8.dp)
                         .align(Alignment.TopStart),
-                    // color = Color.Black.copy(alpha = 0.7f),
-                    color = when {
-                        movie.reviews.map { it.rating }.average() >= 9 ->
-                            nineStars
-                        movie.reviews.map { it.rating }.average() >= 8 ->
-                            eightStars
-                        movie.reviews.map { it.rating }.average() >= 7 ->
-                            sevenStars
-                        movie.reviews.map { it.rating }.average() >= 6 ->
-                            sixStars
-                        movie.reviews.map { it.rating }.average() >= 5 ->
-                            fiveStars
-                        movie.reviews.map { it.rating }.average() >= 4 ->
-                            fourStars
-                        movie.reviews.map { it.rating }.average() >= 3 ->
-                            threeStars
-                        movie.reviews.map { it.rating }.average() >= 2 ->
-                            twoStars
-                        movie.reviews.map { it.rating }.average() >= 1 ->
-                            oneStar
-                        else ->
-                            GrayFaded
-                    },
+                    color = getRatingColor(averageRating),
                     shape = RoundedCornerShape(4.dp)
                 ) {
-                    Text(
-                        text = String.format("%.1f", movie.reviews.map { it.rating }.average()),
-                        style = MaterialTheme.typography.bodySmall,
-                        color = when {
-                            movie.reviews.map { it.rating }.average() in (5.0 .. 7.0) ->
-                                DarkFaded
-                            else ->
-                                Color.White
-                        },
-                        modifier = Modifier.padding(4.dp)
-                    )
+                    Text(text = String.format("%.1f", averageRating), style = MaterialTheme.typography.bodySmall, color = getRatingTextColor(averageRating), modifier = Modifier.padding(4.dp))
                 }
             }
-
-            Text(
-                text = "${movie.reviews.size} reviews",
-                style = MaterialTheme.typography.bodySmall,
-                color = Color.White
-            )
+            Text(text = "${movie.reviews.size} reviews", style = MaterialTheme.typography.bodySmall, color = Color.White)
         }
     }
 
+    private fun getRatingColor(rating: Double): Color {
+        return when {
+            rating >= 9 -> nineStars
+            rating >= 8 -> eightStars
+            rating >= 7 -> sevenStars
+            rating >= 6 -> sixStars
+            rating >= 5 -> fiveStars
+            rating >= 4 -> fourStars
+            rating >= 3 -> threeStars
+            rating >= 2 -> twoStars
+            rating >= 1 -> oneStar
+            else -> GrayFaded
+        }
+    }
+
+    private fun getRatingTextColor(rating: Double): Color {
+        return if (rating in 5.0..7.0) DarkFaded else Color.White
+    }
 }
