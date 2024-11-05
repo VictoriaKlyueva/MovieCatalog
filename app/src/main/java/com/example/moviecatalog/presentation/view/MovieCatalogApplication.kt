5327b0ee1b2df13b960b2e3ac4f3ddc3322505f4
vekosek <vika_klyueva@inbox.ru>
2024-11-05 20:38:04 +0700

    Add to favorite fix

--- app/src/main/java/com/example/moviecatalog/presentation/view/MovieCatalogApplication.kt
+++ app/src/main/java/com/example/moviecatalog/presentation/view/MovieCatalogApplication.kt
@@ -2,7 +2,6 @@ package com.example.moviecatalog.presentation.view
 
 import android.app.Application
 import com.example.moviecatalog.data.api.client.ApiClient
-import com.example.moviecatalog.data.datasource.TokenDataSource
 
 class MovieCatalogApplication : Application() {
     override fun onCreate() {
