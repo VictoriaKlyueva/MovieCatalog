6789d3bff34e0bb825ff07ef1af314095b47923d
vekosek <vika_klyueva@inbox.ru>
2024-11-05 22:55:31 +0700

    Fixed favorites

--- app/src/main/java/com/example/moviecatalog/presentation/viewModel/MoviesViewModel.kt
+++ app/src/main/java/com/example/moviecatalog/presentation/viewModel/MoviesViewModel.kt
@@ -1,72 +1,65 @@
-    package com.example.moviecatalog.presentation.viewModel
+package com.example.moviecatalog.presentation.viewModel
 
-    import androidx.lifecycle.LiveData
-    import androidx.lifecycle.MutableLiveData
-    import androidx.lifecycle.ViewModel
-    import com.example.moviecatalog.data.model.MovieElementModel
-    import com.example.moviecatalog.data.repository.FavoriteMoviesRepositoryImpl
-    import com.example.moviecatalog.data.repository.MovieRepositoryImpl
-    import com.example.moviecatalog.domain.usecase.GetFavoritesMoviesUseCase
-    import com.example.moviecatalog.domain.usecase.GetMoviesFromPageUseCase
-    import com.example.moviecatalog.domain.usecase.GetRandomMovieUseCase
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.moviecatalog.data.model.MovieElementModel
+import com.example.moviecatalog.data.repository.FavoriteMoviesRepositoryImpl
+import com.example.moviecatalog.data.repository.MovieRepositoryImpl
+import com.example.moviecatalog.domain.conmmon.Constants.TOTAL_PAGES
+import com.example.moviecatalog.domain.usecase.CheckFavoriteMovieUseCase
+import com.example.moviecatalog.domain.usecase.GetFavoritesMoviesUseCase
+import com.example.moviecatalog.domain.usecase.GetMoviesFromPageUseCase
+import com.example.moviecatalog.domain.usecase.GetRandomMovieUseCase
 
-    class MoviesViewModel : ViewModel() {
-        private val movieRepository = MovieRepositoryImpl()
-        private val movieResponseUseCase = GetMoviesFromPageUseCase(movieRepository)
-        private val getRandomMovieUseCase = GetRandomMovieUseCase(movieRepository)
+class MoviesViewModel : ViewModel() {
+    private val movieRepository = MovieRepositoryImpl()
+    private val movieResponseUseCase = GetMoviesFromPageUseCase(movieRepository)
+    private val getRandomMovieUseCase = GetRandomMovieUseCase(movieRepository)
 
-        private val favoritesMoviesRepository = FavoriteMoviesRepositoryImpl()
-        private val favoritesMoviesUseCase = GetFavoritesMoviesUseCase(favoritesMoviesRepository)
+    private val favoritesMoviesRepository = FavoriteMoviesRepositoryImpl()
+    private val checkFavoriteMovieUseCase = CheckFavoriteMovieUseCase(favoritesMoviesRepository)
+    private val favoritesMoviesUseCase = GetFavoritesMoviesUseCase(favoritesMoviesRepository)
 
-        private val _movies = MutableLiveData<List<MovieElementModel>>()
-        val movies: LiveData<List<MovieElementModel>> get() = _movies
+    private val _movies = MutableLiveData<List<MovieElementModel>>()
+    val movies: LiveData<List<MovieElementModel>> get() = _movies
 
-        private val _favoritesMovies = MutableLiveData<List<MovieElementModel>>()
-        val favoritesMovies: LiveData<List<MovieElementModel>> get() = _favoritesMovies
+    private val _favoritesMovies = MutableLiveData<List<MovieElementModel>>()
+    val favoritesMovies: LiveData<List<MovieElementModel>> get() = _favoritesMovies
 
-        private val _movie = MutableLiveData<MovieElementModel>()
-        val movie: LiveData<MovieElementModel> get() = _movie
+    private val _movie = MutableLiveData<MovieElementModel>()
+    val movie: LiveData<MovieElementModel> get() = _movie
 
-        fun getRandomMovie(callback: (MovieElementModel?, String?) -> Unit) {
-            getRandomMovieUseCase.execute { randomMovie, error ->
-                if (randomMovie != null) {
-                    _movie.postValue(randomMovie)
-                    callback(randomMovie, null)
-                } else {
-                    callback(null, error)
-                }
-            }
-        }
-
-        fun fetchMovies(page: Int = (1..5).random()) {
-            movieResponseUseCase.execute(page) { movies, error ->
-                if (error == null) {
-                    _movies.postValue(movies?.take(5) ?: emptyList())
-                } else {
-                    println("Ошибка получения данных: $error")
-                    _movies.postValue(emptyList())
-                }
-            }
-        }
-
-        fun fetchFavoriteMovies() {
-            /*
-            favoritesMoviesUseCase.execute { movies, error ->
-                if (error == null) {
-                    _favoritesMovies.postValue(movies ?: emptyList())
-                } else {
-                    _favoritesMovies.postValue(emptyList())
-                }
-            }
-            */
-            val page = (1..5).random()
-            movieResponseUseCase.execute(page) { movies, error ->
-                if (error == null) {
-                    _favoritesMovies.postValue(movies?.take(5) ?: emptyList())
-                } else {
-                    println("Ошибка получения данных: $error")
-                    _favoritesMovies.postValue(emptyList())
-                }
+    fun getRandomMovie(callback: (MovieElementModel?, String?) -> Unit) {
+        getRandomMovieUseCase.execute { randomMovie, error ->
+            if (randomMovie != null) {
+                _movie.postValue(randomMovie)
+                callback(randomMovie, null)
+            } else {
+                callback(null, error)
             }
         }
     }
+
+    fun fetchMovies(page: Int = (1..TOTAL_PAGES).random()) {
+        movieResponseUseCase.execute(page) { movies, error ->
+            if (error == null) {
+                _movies.postValue(movies?.take(5) ?: emptyList())
+            } else {
+                println("Ошибка получения данных: $error")
+                _movies.postValue(emptyList())
+            }
+        }
+    }
+
+    fun fetchFavoriteMovies() {
+        favoritesMoviesUseCase.execute { movies, error ->
+            if (error == null) {
+                _favoritesMovies.postValue(movies ?: emptyList())
+            } else {
+                println("Ошибка получения данных: $error")
+                _favoritesMovies.postValue(emptyList())
+            }
+        }
+    }
+}
