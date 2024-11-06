29ddb58fe3dacd808aee43e3808a0621b2dfe13c
vekosek <vika_klyueva@inbox.ru>
2024-11-06 15:49:15 +0700

    Splash fixed

--- app/src/main/java/com/example/moviecatalog/presentation/view/SplashScreen/SplashActivity.kt
+++ app/src/main/java/com/example/moviecatalog/presentation/view/SplashScreen/SplashActivity.kt
@@ -8,25 +8,14 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.moviecatalog.R
 import com.example.moviecatalog.presentation.view.MainActivity
-import com.example.moviecatalog.presentation.viewModel.SplashViewModel
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
-
-    private lateinit var viewModel: SplashViewModel
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.splash_activity)
 
-        viewModel = ViewModelProvider(this)[SplashViewModel::class.java]
-
-        viewModel.navigateToMain.observe(this, Observer { navigate ->
-            if (navigate == true) {
-                navigateToMainActivity()
-                viewModel.onNavigationComplete()
-            }
-        })
+        navigateToMainActivity()
     }
 
     private fun navigateToMainActivity() {
